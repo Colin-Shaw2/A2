7bba4539d7d0600ca2a7cb8b883e7928d59640d1
Colin-Shaw2 <colin.shaw2@uoit.net>
2018-03-27 18:01:57 -0400

    started server and ClientConnetionHandler

---
+++ src/sample/Server.java
@@ -0,0 +1,26 @@
+package sample;
+
+import java.io.*;
+import java.net.*;
+import java.util.Vector;
+
+public class Server implements Runnable{
+    protected ServerSocket serverSocket;
+    protected String ServerPath;
+
+    public static int SERVER_PORT = 16789;
+    public static int MAX_CLIENTS = 4;
+
+    public Server() {
+        try {
+            serverSocket = new ServerSocket();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        
+    }
+}
